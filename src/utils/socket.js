0e937ef161debc7df92ab7a68065e567f255267a
very5573 <tripathivery@gmail.com>
2025-12-09 18:35:41 +0530

    Delete src directory

--- src/utils/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000", {
-  transports: ["websocket"], // ⚡ Important: Use websocket transport
-  withCredentials: true,      // If backend uses credentials: true
-});
-
-export default socket;
